034cb1ec6b35e97cce110730a17497c2e0bc889b
Jean-Francois Brouillet <verec@mac.com>
2014-05-15 23:00:26 +0100

    initial push

---
+++ Host/Host/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Host
+//
+//  Created by verec on 15/05/2014.
+//  Copyright (c) 2014 CantabiLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
